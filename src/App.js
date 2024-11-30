578a9d1d40ae14176a6deaec7182286519c39771
fitsum <belay.fitsum@gmail.com>
2024-11-30 23:29:31 +0200

    adding all files from local

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          react and docker awsome love it!
         </p>
         <a
           className="App-link"
